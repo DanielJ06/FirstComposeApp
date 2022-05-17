842c5398bca4ae8f7827b385c5efc8f0ccdcfda7
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-05-16 21:19:46 -0300

    Create ImageCard

--- app/src/main/java/com/example/first/compose/app/MainActivity.kt
+++ app/src/main/java/com/example/first/compose/app/MainActivity.kt
@@ -3,34 +3,31 @@ package com.example.first.compose.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import com.example.first.compose.app.ui.random_composables.Conversation
-import com.example.first.compose.app.ui.random_composables.Message
-import com.example.first.compose.app.ui.theme.FirstComposeAppTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.first.compose.app.ui.random_composables.ImageCard
 
 class MainActivity : ComponentActivity() {
-
-    private val mockedConversation = listOf(
-        Message("Daniel J", "O que achou do compose?"),
-        Message("Daniel R", "As primeiras impressões foram bacanas"),
-        Message("Daniel J", "Lembra um pouco o React Native, né?"),
-        Message("Daniel R", "Até que lembra sim")
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Conversation(messages = mockedConversation)
-                }
+            val painter = painterResource(id = R.drawable.dog)
+            val title = "I think that's what happens when a bee bites a dog"
+            val description = "Cutest dog ever"
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(8.dp)
+            ) {
+                ImageCard(
+                    painter = painter, title = title, contentDescription = description
+                )
             }
         }
     }
